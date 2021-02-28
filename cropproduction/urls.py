03c53ac3c79e4c570829557d84aee753badb9c3b
Hari95-creator <krishnansg95@gmail.com>
2021-02-28 22:16:51 +0530

    mini-project

---
+++ cropproduction/urls.py
@@ -0,0 +1,57 @@
+"""cropproduction URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from farmers import views
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('addproduct/',views.addproduct),
+    path('dashboard/',views.dash),
+    path('editproduct/',views.editproduct),
+    path('login/',views.login),
+    path('loginsignup/',views.loginsignup),
+    path('orderdetails/',views.orderdetails),
+    path('orderhistory/',views.orderhistory),
+    path('orders/',views.orders),
+    path('productdetails/',views.productdetails),
+    path('signup/',views.signup),
+
+    #public view  path
+    path('publicabout/',views.publicabout),
+    path('publiccart/',views.publiccart),
+    path('changeaddress/',views.changeaddress),
+    path('changepassword/',views.changepassword),
+    path('publiccheckout/',views.publiccheckout),
+    path('publiccontact/',views.publiccontact),
+    #path('publicindex/',views.publicindex),
+    url(r'^$',views.publicindex),#for creating root page 
+    path('publiclogin/',views.publiclogin),
+    path('publicorderstatus/',views.orderstatus),
+    path('publicorderhistory/',views.orderhistory),
+    path('farmersdetails/',views.ourfarmersdetails),
+    path('farmers/',views.ourfarmers),
+    path('publicproduct/',views.publicproduct),
+    path('publicshop/',views.publicshop),
+    path('publicsignup/',views.publicsignup),
+    path('publicwishlist/',views.publicwishlist),
+    path('errorpage/',views.errorpage),
+    url(r'^ajax/autocomplete/$', views.autocomplete, name='ajax_autocomplete'),
+    
+
+
+]
---
+++ farmers/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render
+
+# Create your views here.
+def addproduct(request):
+    return render(request,'farmers/addproduct.html')
+def dash(reguest):
+    return render(reguest,'farmers/dashboard.html')
+def editproduct(request):
+    return render(request,'farmers/editproduct.html')
+def login(request):
+    return render(request,'farmers/login.html')
+def loginsignup(request):
+    return render(request,'farmers/loginsignupbase.html')
+def orderdetails(request):
+    return render(request,'farmers/orderdetails.html')
+def orderhistory(request):
+    return render(request,'farmers/orderhistory.html')
+def orders(request):
+    return render(request,'farmers/orders.html')
+def productdetails(request):
+    return render(request,'farmers/productdetails.html')
+def signup(request):
+    return render(request,'farmers/signup.html')
+
+#functions for public
+
+def autocomplete(request):
+    if request.is_ajax():
+        queryset = products.objects.filter(pname__icontains=request.GET.get('search', None))
+        list = []        
+        for i in queryset:
+            list.append(i.pname)
+        data = {
+            'list': list,
+        }
+        return JsonResponse(data)
+
+def publicabout(request):
+    return render(request,'public/about.html')
+def publiccart(request):
+    return render(request,'public/cart.html')
+def changeaddress(request):
+    return render(request,'public/change_address.html')
+def changepassword(request):
+    return render(request,'public/change_password.html')
+def publiccheckout(request):
+    return render(request,'public/checkout.html')
+def publiccontact(request):
+    return render(request,'public/contact.html')
+def publicindex(request):
+    return render(request,'public/index.html')
+def publiclogin(request):
+    return render(request,'public/login.html')
+def orderstatus(request):
+    return render(request,'public/order_status.html')
+def orderhistory(request):
+    return render(request,'public/orderhistory.html')
+def ourfarmersdetails(request):
+    return render(request,'public/ourfarmers_details.html')
+def ourfarmers(request):
+    return render(request,'public/ourfarmers.html')
+def publicproduct(request):
+    return render(request,'public/product.html')
+def publicshop(request):
+    return render(request,'public/shop.html')
+def publicsignup(request):
+    return render(request,'public/signup.html')  
+def publicwishlist(request):
+    return render(request,'public/wishlist.html')
+def errorpage(request):
+    return render(request,'public/4O4.html')
+
+
+
+
